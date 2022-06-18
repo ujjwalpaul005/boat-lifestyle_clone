19940a01e505d0b4630204e2d58171d4c553c71b
iam-souvik <souvikpatramax@gmail.com>
2022-06-18 20:48:32 +0530

    navbar added to home page

---
+++ navbar.js
@@ -0,0 +1,34 @@
+
+   
+
+    function showLogin() {
+        let box = document.createElement("div")
+        box.setAttribute("id", "showLogIn")
+        let headline = document.createElement("div")
+        let boatHead = document.createElement("h3")
+        boatHead.innerText = "Hey boAthead!"
+
+        let button = document.createElement("button")
+        button.innerText = "X"
+        button.addEventListener("click", deleteOption);
+
+        let loginbtn = document.createElement("button")
+        loginbtn.innerText = "Log in"
+        loginbtn.addEventListener("click", function () {
+            window.location.href = "login.html";
+        })
+
+        headline.append(boatHead, button)
+        box.append(headline, loginbtn)
+        document.querySelector("body").append(box)
+
+
+    }
+
+    function deleteOption() {
+        document.querySelector("#showLogIn").remove();
+    }
+
+
+
+
